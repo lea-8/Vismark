2c7c02470641068edf7886ce622f494cf4d4878d
Lea <s1953043@ed.ac.uk>
2025-08-01 23:35:24 +0100

    Fix double maths formula with global styling wrapped in mdsvex layout

---
+++ src/lib/components/text/Dropdown.svelte
@@ -0,0 +1,12 @@
+<script>
+  let {
+    shownText = ""
+  } = $props();
+</script>
+
+<!-- svelte-ignore slot_element_deprecated -->
+<details>
+<summary>{shownText}</summary>
+<br>
+<slot/>
+</details>
