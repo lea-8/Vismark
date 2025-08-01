3a8554272dc5367b1660071dc46057c04ab54761
Lea <s1953043@ed.ac.uk>
2025-08-01 20:28:25 +0100

    Add maths support plugin

--- src/lib/preprocessorConfigHelper.js
+++ src/lib/preprocessorConfigHelper.js
@@ -1,5 +1,10 @@
 import { mdsvex } from 'mdsvex';
+
+// plugins:
 import remarkFootnotes from 'remark-footnotes';
+import math from 'remark-math'
+import rehype_katex from 'rehype-katex';
+import { katex_blocks, correct_hast_tree } from './plugins/maths.js';
 
 /**
  * A helper to set up mdsvex + Svelte preprocessing.
@@ -12,7 +17,8 @@ export function vismarkConfig(baseConfig = {}, mdsvexOptions = {}) {
     preprocess: [
       mdsvex({
         extensions: ['.vismd'],
-        remarkPlugins: [remarkFootnotes],
+        remarkPlugins: [remarkFootnotes, math, katex_blocks],
+        rehypePlugins: [correct_hast_tree, rehype_katex],    
         ...mdsvexOptions 
       })
     ],
