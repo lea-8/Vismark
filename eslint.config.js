380cdb2994e268f0412d6e4d8cf38320c705324f
Lea <s1953043@ed.ac.uk>
2025-08-13 14:08:44 +0100

    Finish ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,30 @@ import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  globalIgnores([
+		"!node_modules/", // unignore `node_modules/` directory
+		"node_modules/*", // ignore its content
+		"!node_modules/mylibrary/", // unignore `node_modules/mylibrary` directory
+		".svelte-kit/*",
+    "src/lib/styles/*",
+	]),
   { 
-    files: ["**/*.md"], 
+    files: ["src/lib/*.{js,mjs,cjs}"], 
+    plugins: { js }, extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser },
+    ignores: ["src/lib/styles/**"]
+  },
+  { 
+    files: ["package.json"], 
+    plugins: { json }, 
+    language: "json/json", 
+    extends: ["json/recommended"] 
+  },
+  { 
+    files: ["**/*.{md, vismd}"], 
     plugins: { markdown }, 
     language: "markdown/commonmark", 
     extends: ["markdown/recommended"],
@@ -17,5 +34,5 @@ export default defineConfig([
       "markdown/no-missing-label-refs": "warn"
     }
   },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  // { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
